149ad9ce265616f7c000894b6526ffcaae8d15f5
Joost van den Broek <jvandenbroek@gmail.com>
2018-05-22 09:56:37 +0000

    fix stdafx

--- main/SQLHelper/SQLUpdateFunctions.cpp
+++ main/SQLHelper/SQLUpdateFunctions.cpp
@@ -1,4 +1,4 @@
-#include "../stdafx.h"
+#include "stdafx.h"
 #include "../SQLHelper.h"
 #include "../localtime_r.h"
 #include "../Logger.h"
--- main/mainworker/mainworker_decode.cpp
+++ main/mainworker/mainworker_decode.cpp
@@ -1,5 +1,5 @@
 
-#include "../stdafx.h"
+#include "stdafx.h"
 #include "../mainworker.h"
 #include "../localtime_r.h"
 #include "../WebServerHelper.h"
